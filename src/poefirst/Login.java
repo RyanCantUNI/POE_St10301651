2fc91ca1c762052a2202099d2d6141fa1bb2be7b
unknown <ST10301651@vcconnect.edu.za>
2023-05-02 17:27:45 +0000

    V3

--- src/poefirst/Login.java
+++ src/poefirst/Login.java
@@ -4,13 +4,77 @@ import java.util.Scanner;
 import java.util.regex.*;
 import java.util.*;
 import java.awt.Component;
+
 /**
  *
  * @author lab_services_student
  */
-public class POEfirst {
+public class Login {
 
-    /**
-     * @param args the command line arguments
-     */
+     public boolean checkUserName(String username) {
+         
+        return username.length() >= 5 && username.contains("_");
+    }
+
+    public  boolean checkPasswordComplexity(String password) {
+
+        Pattern letter = Pattern.compile("[A-Z]");
+        Pattern digit = Pattern.compile("[0-9]");
+        Pattern special = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        //Pattern eight = Pattern.compile (".{8}");
+
+        //cHECKING IF THE PASSWORD ACTUALLY MATCHESD THE CONDITIIONS WORKS
+        Matcher hasLetter = letter.matcher(password);
+        Matcher hasDigit = digit.matcher(password);
+        Matcher hasSpecial = special.matcher(password);
+
+        if (password.length() <= 8) {
+            return false;
+        }
+
+        return hasLetter.find() && hasDigit.find() && hasSpecial.find();
+
+    }
+
+    public  String registerUser(boolean validateUserName, boolean validatePassword) {
+
+        if (validateUserName == false) {
+
+            return "Username is not correctly formatted, please ensure that your username contains an underscore and is no more than 5 characters in length .";
+        }
+        if (validatePassword == false) {
+
+            return "Password is not correctly formatted, please ensure that the password contains at least 8 characters, a capital letter, a number and a special character";
+        }
+
+        return "Password successfully captured";
+
+    }
+    
+    public  boolean loginUser(String username, String password) {
+        Scanner logScan = new Scanner(System.in);
+        System.out.println("--Login--");
+
+        System.out.println("Enter username: ");
+        String loginUsername = logScan.nextLine();
+
+        System.out.println("Enter password: ");
+        String loginPassword = logScan.nextLine();
+
+        if (!username.contains(loginUsername)) {
+            return false;
+        }
+        if (!password.contains(loginPassword)) {
+            return false;
+        }
+        return true;
+    }
+
+    public  String returnLoginStatus(boolean loginStatus, String firstName, String lastName) {
+        if (!loginStatus) {
+            return "Username or password incorrect, please try again!";
+        }
+        return "Welcome " + firstName + ", " + lastName + " it is great to see you again!!";
+
+    }
 }
---
+++ test/poefirst/LoginTest.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package poefirst;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class LoginTest {
+    
+    public LoginTest() {
+    }
+
+    /**
+     * Test of checkUserName method, of class Login.
+     */
+    @Test
+    public void testCheckUserName() {
+        System.out.println("checkUserName");
+        String username = "kyl_1";
+        Login instance = new Login();
+        boolean expResult = true;
+        boolean result = instance.checkUserName(username);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of checkPasswordComplexity method, of class Login.
+     */
+    @Test
+    public void testCheckPasswordComplexity() {
+        System.out.println("checkPasswordComplexity");
+        String password = "Ch&&sec@ke99!";
+        Login instance = new Login();
+        boolean expResult = true;
+        boolean result = instance.checkPasswordComplexity(password);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of registerUser method, of class Login.
+     */
+    @Test
+    public void testRegisterUser() {
+        System.out.println("registerUser");
+        boolean validateUserName = true;
+        boolean validatePassword = true;
+        Login instance = new Login();
+        String expResult = "Password successfully captured";
+        String result = instance.registerUser(validateUserName, validatePassword);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of loginUser method, of class Login.
+     */
+    @Test
+    public void testLoginUser() {
+        System.out.println("loginUser");
+        String username = "kyl_1";
+        String password = "Ch&&sec@ke99!";
+        Login instance = new Login();
+        boolean expResult = true;
+        boolean result = instance.loginUser(username, password);
+        assertEquals(expResult, result);
+        
+    }
+
+    /**
+     * Test of returnLoginStatus method, of class Login.
+     */
+    @Test
+    public void testReturnLoginStatus() {
+        System.out.println("returnLoginStatus");
+        boolean loginStatus = true;
+        String firstName = "";
+        String lastName = "";
+        Login instance = new Login();
+        String expResult = "Welcome " + firstName + ", " + lastName + " it is great to see you again!!";
+        String result = instance.returnLoginStatus(loginStatus, firstName, lastName);
+        assertEquals(expResult, result);
+        
+    }
+    
+}
